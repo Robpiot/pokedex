c0777eaa1f9a60b93d2388188997c25f856e5e6f
Robpiot <robin.piot@outlook.com>
2023-11-16 16:15:16 +0100

    aled jaime pas les integers

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
+
 ?>
 
 
@@ -21,5 +22,35 @@ ini_set("display_errors", 1);
   </nav>
   <h1>Welcome to my Pokedex!</h1>
   <p>Here's where I keep track of all my Pokemon.</p>
+
+  <form method="POST" action="index.php">
+  <label for="pkmName">Pokemon Name:</label>
+  <input type="text" id="pkmName" name="pkmName" required>
+
+  <label for="id">Pokemon ID:</label>
+  <input type="number" id="id" name="id" required>
+
+  <label for="type1">Pokemon Type:</label>
+  <input type="text" id="type1" name="type1" required>
+
+  <label for="type2"> Pokemon Type 2:</label>
+  <input type="text" id="type2" name="type2">
+
+  <label for="bio">Pokemon Bio:</label>
+  <textarea id="bio" name="bio" required></textarea>
+
+  <label for="evolution1">First Evolution id:</label>
+  <input type="number" id="evolution1" name="evolution1">
+
+  <label for="evolution2">Second Evolution id:</label>
+  <input type="number" id="evolution2" name="evolution2">
+
+  <label for="img">Pokemon Image:</label>
+  <input type="text" id="img" name="img">
+
+  <label for="send"></label>
+    <input type="submit" id="send" name="send" value="Register">
+    <?php require './scriptsphp/addPokemon.php'; ?>
+</form>
 </body>
 </html>
---
+++ scriptsphp/addPokemon.php
@@ -0,0 +1,29 @@
+<?php
+require './connect.php';
+
+try {
+    if (isset($_POST['send'])) {
+        $name = $_POST['pkmName'];
+        $id = $_POST['id'];
+        $type1 = $_POST['type1'];
+        $type2 = $_POST['type2'] ?? null; // If type2 is not set or is null, it will be set to null
+        $bio = $_POST['bio'];
+        $evolution1 = $_POST['evolution1'] ?? null; // If evolution1 is not set or is null, it will be set to null
+        $evolution2 = $_POST['evolution2'] ?? null; // If evolution2 is not set or is null, it will be set to null
+        $img = $_POST['img'] ?? null;
+
+        $stmt = $pdo->prepare("INSERT INTO pokedex (pkmName, id, type1, type2, bio, evolution1, evolution2, img) VALUES (:pkmName, :id, :type1, :type2, :bio, :evolution1, :evolution2, :img)");
+        $stmt->bindParam(':pkmName', $name);
+        $stmt->bindParam(':id', $id);
+        $stmt->bindParam(':type1', $type1);
+        $stmt->bindParam(':type2', $type2);
+        $stmt->bindParam(':bio', $bio);
+        $stmt->bindParam(':evolution1', $evolution1);
+        $stmt->bindParam(':evolution2', $evolution2);
+        $stmt->bindParam(':img', $img);
+        $stmt->execute();
+    }
+} catch (Exception $e) {
+    // In case of error, display a message and stop everything
+    die('Error : ' . $e->getMessage());
+}
