cd3d61c2b58c58ed09d55a5f588da22c7316f41d
Robpiot <robin.piot@outlook.com>
2023-11-17 10:02:04 +0100

    pierre bouge toi le cul et merge

--- scriptsphp/addPokemon.php
+++ scriptsphp/addPokemon.php
@@ -5,12 +5,12 @@ try {
     if (isset($_POST['send'])) {
         $name = $_POST['pkmName'];
         $id = $_POST['id'];
-        $type1 = $_POST['type1'];
-        $type2 = $_POST['type2'] ?? null; // If type2 is not set or is null, it will be set to null
-        $bio = $_POST['bio'];
+        $type1 = !empty($_POST['type1']) ? $_POST['type1'] : NULL;
+        $type2 = !empty($_POST['type2']) ? $_POST['type2'] : NULL;// If type2 is not set or is null, it will be set to null
+        $bio = !empty($_POST['bio']) ? $_POST['bio'] : NULL;
         $evolution1 = !empty($_POST['evolution1']) ? $_POST['evolution1'] : NULL; 
 $evolution2 = !empty($_POST['evolution2']) ? $_POST['evolution2'] : NULL;
-        $img = $_POST['img'] ?? null;
+        $img = !empty($_POST['img']) ? $_POST['img'] : NULL;
 
         $stmt = $pdo->prepare("INSERT INTO pokedex (pkmName, id, type1, type2, bio, evolution1, evolution2, img) VALUES (:pkmName, :id, :type1, :type2, :bio, :evolution1, :evolution2, :img)");
         $stmt->bindParam(':pkmName', $name);
