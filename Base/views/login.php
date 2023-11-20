c2f524174b6b246bf5ebe5d1ca5f5667a0244489
Valentin-Lefort <lefortvalentin94@gmail.com>
2023-11-20 11:47:12 +0100

    style login

--- Base/views/login.php
+++ Base/views/login.php
@@ -6,14 +6,15 @@ require_once __DIR__ . '/partials/header.php';
 ?>
 
 <div class="logindiv">
+  <h2>My Account</h2>
   <form method="POST" action="login.php">
-    <label for="username">Username:</label>
+    <label for="username">Username *</label>
     <input type="text" id="username" name="username" placeholder="eg. OndineDu38" required>
 
-    <label for="password">Password:</label>
+    <label for="password">Password *</label>
     <input type="password" id="password" name="password" placeholder="My password" required>
 
-    <button type="submit">Log in</button>
+    <button type="submit">Connexion</button>
   </form>
 </div>
 <?php
